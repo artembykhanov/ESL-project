6b98271c210b8a44c95db3c9073ed3804a2ae77e
Artem Bykhanov <rigli.fx@gmail.com>
2024-10-29 16:11:40 +0000

    Workshop 3: Use GPIO

--- main.c
+++ main.c
@@ -1,85 +1,128 @@
-/** @file
- *
- * @defgroup blinky_example_main main.c
- * @{
- * @ingroup blinky_example
- * @brief Blinky Example Application main file.
- *
- * This file contains the source code for a sample application to blink LEDs.
- *
- */
-
 #include <stdbool.h>
 #include <stdint.h>
 #include "nrf_delay.h"
 #include "boards.h"
+#include "nrf_gpio.h"
 
 #define DEVICE_ID 6585
-#define LED_ON_TIME_MS 400
-#define LED_OFF_TIME_MS 400
-#define PAUSE_BETWEEN_LEDS 2000
-#define PAUSE_AFTER_ID_MS 3000
+#define LED_ON_TIME_MS 1000
+#define LED_OFF_TIME_MS 1000
+#define PAUSE_AFTER_ID_MS 300
+
+#define LED_PIN NRF_GPIO_PIN_MAP(0, 6)
+#define LED_R_PIN NRF_GPIO_PIN_MAP(0, 8)
+#define LED_G_PIN NRF_GPIO_PIN_MAP(1, 9)
+#define LED_B_PIN NRF_GPIO_PIN_MAP(0, 12)
+
+#define BUTTON_PIN NRF_GPIO_PIN_MAP(1, 6)
 
 void led_blink(int i);
+void init_pin();
+void init_led_arr(int *led_arr, int len_led_arr);
+bool is_button_press();
+void end_blink();
 
-/**
- * @brief Function for application main entry.
- */
+const int digit1 = DEVICE_ID / 1000;
+const int digit2 = (DEVICE_ID / 100) % 10;
+const int digit3 = (DEVICE_ID / 10) % 10;
+const int digit4 = DEVICE_ID % 10;
+
 int main(void)
 {
-    /* Configure board. */
     bsp_board_init(BSP_INIT_LEDS);
+    bsp_board_init(BSP_INIT_BUTTONS);
+    init_pin();
 
-    // My device id #6585
+    const int len_led_arr = digit1 + digit2 + digit3 + digit4;
+    int led_arr[len_led_arr];
 
-    int digit1 = DEVICE_ID / 1000;
-    int digit2 = (DEVICE_ID / 100) % 10;
-    int digit3 = (DEVICE_ID / 10) % 10;
-    int digit4 = DEVICE_ID % 10;
+    init_led_arr(led_arr, len_led_arr);
 
-    /* Toggle LEDs. */
+    int index = 0;
+
     while (true)
     {
-
-        for (int i = 0; i < digit1; i++)
+        if (is_button_press())
         {
-            led_blink(0);
+            bsp_board_leds_off();
+            nrf_delay_ms(LED_OFF_TIME_MS / 2);
+            led_blink(led_arr[index++]);
+            if (index >= len_led_arr)
+            {
+                nrf_delay_ms(LED_OFF_TIME_MS);
+                end_blink();
+                index = 0;
+            }
         }
-        nrf_delay_ms(PAUSE_BETWEEN_LEDS);
-
-        for (int i = 0; i < digit2; i++)
+        else
         {
-            led_blink(1);
+            nrf_delay_ms(100);
         }
-        nrf_delay_ms(PAUSE_BETWEEN_LEDS);
-
-        for (int i = 0; i < digit3; i++)
-        {
-            led_blink(2);
-        }
-        nrf_delay_ms(PAUSE_BETWEEN_LEDS);
-
-        for (int i = 0; i < digit4; i++)
-        {
-            led_blink(3);
-        }
-
-        // after displaying the ID, turn on all the LEDs, this means the end of the program
-        nrf_delay_ms(PAUSE_AFTER_ID_MS);
-        bsp_board_leds_on();
-        nrf_delay_ms(PAUSE_BETWEEN_LEDS);
-        bsp_board_leds_off();
-        nrf_delay_ms(PAUSE_AFTER_ID_MS);
     }
 }
 
-void led_blink(int i)
+void led_blink(int pin)
 {
-    bsp_board_led_invert(i);
-    nrf_delay_ms(LED_ON_TIME_MS);
-    bsp_board_led_invert(i);
-    nrf_delay_ms(LED_OFF_TIME_MS);
+    nrf_gpio_pin_write(pin, 0);
+
+    for (int i = 0; i < LED_ON_TIME_MS / 100; i++)
+    {
+        if (!is_button_press())
+        {
+            return;
+        }
+        nrf_delay_ms(100);
+    }
+
+    nrf_gpio_pin_write(pin, 1);
+    nrf_delay_ms(LED_OFF_TIME_MS / 2);
 }
-/**
- *@}
- **/
+
+void init_led_arr(int *led_arr, int len_led_arr)
+{
+    int index = 0;
+
+    for (int i = 0; i < digit1; i++)
+    {
+        led_arr[index++] = LED_PIN;
+    }
+    for (int i = 0; i < digit2; i++)
+    {
+        led_arr[index++] = LED_R_PIN;
+    }
+    for (int i = 0; i < digit3; i++)
+    {
+        led_arr[index++] = LED_G_PIN;
+    }
+    for (int i = 0; i < digit4; i++)
+    {
+        led_arr[index++] = LED_B_PIN;
+    }
+}
+
+void init_pin()
+{
+    nrf_gpio_cfg_output(LED_PIN);
+    nrf_gpio_cfg_output(LED_R_PIN);
+    nrf_gpio_cfg_output(LED_G_PIN);
+    nrf_gpio_cfg_output(LED_B_PIN);
+    nrf_gpio_cfg_input(BUTTON_PIN, NRF_GPIO_PIN_PULLUP);
+}
+
+bool is_button_press()
+{
+    return nrf_gpio_pin_read(BUTTON_PIN) == 0;
+}
+
+void end_blink()
+{
+    bsp_board_leds_on();
+    nrf_delay_ms(PAUSE_AFTER_ID_MS);
+    bsp_board_leds_off();
+    nrf_delay_ms(PAUSE_AFTER_ID_MS);
+    bsp_board_leds_on();
+    nrf_delay_ms(PAUSE_AFTER_ID_MS);
+    bsp_board_leds_off();
+    nrf_delay_ms(PAUSE_AFTER_ID_MS);
+    nrf_delay_ms(LED_ON_TIME_MS);
+}
